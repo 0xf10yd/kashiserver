a1ec07c0373cfc00336b42dd3d3f046df2716dca
0xf10yd <0xf10yd@protonmail.com>
2022-04-23 02:28:17 +0200

    Create errorMiddleware.js

---
+++ src/common/errorMiddleware.js
@@ -0,0 +1,9 @@
+const errorHandler = (err, req, res, next) => {
+  let { statusCode, message } = err;
+  if (statusCode === undefined) {
+    statusCode = 500;
+  }
+  res.status(statusCode).send(message);
+};
+
+module.exports = errorHandler;
