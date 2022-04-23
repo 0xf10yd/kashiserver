d0e3a11d720650348edf83da7e1f9f5bc57dc0eb
0xf10yd <0xf10yd@protonmail.com>
2022-04-23 02:28:23 +0200

    Create mongoProvider.js

---
+++ src/common/mongoProvider.js
@@ -0,0 +1,14 @@
+const { MongoClient } = require("mongodb");
+const { databaseConnectionString, databaseName } = require("../config");
+
+const mongoClient = new MongoClient(databaseConnectionString);
+(async () => {
+  await mongoClient.connect();
+})();
+
+const mongoDb = mongoClient.db(databaseName);
+
+module.exports = {
+  mongoDb,
+  mongoClient,
+};
