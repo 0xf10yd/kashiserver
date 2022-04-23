528267e8a248161f479af53208c465b62ade6e47
0xf10yd <0xf10yd@protonmail.com>
2022-04-23 02:27:42 +0200

    global utilities and helper functions

---
+++ src/common/utils.js
@@ -0,0 +1,34 @@
+const assertHasProperty = (obj, prop) => {
+  let has = obj[prop] === null || obj[prop] === undefined;
+  if (has == false) {
+    throw new Error(prop);
+  }
+  return has;
+};
+
+const catchAsync = (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+};
+
+const isNullOrEmpty = (text) => {
+  return text === null || text === undefined || text.length == 0;
+};
+
+const isNull = (text) => {
+  return text === null || text === undefined;
+};
+
+const today = () => {
+  let dt = new Date();
+  const offset = dt.getTimezoneOffset();
+  dt = new Date(dt.getTime() - offset * 60 * 1000);
+  return dt.toISOString().split("T")[0];
+};
+
+module.exports = {
+  assertHasProperty,
+  catchAsync,
+  today,
+  isNullOrEmpty,
+  isNull
+};
